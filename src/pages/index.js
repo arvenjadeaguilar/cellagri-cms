081f1d3f972edd892c7f7a04636c8ed8aa14b9f6
arvenjadeaguilar <arvenjadeaguilar@gmail.com>
2018-02-07 16:16:12 +0800

    feat(mobile): Added mobile screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "gatsby-link";
 import Script from "react-load-script";
 import graphql from "graphql";
+import Navbar from '../components/Navbar';
 
 export default class IndexPage extends React.Component {
   handleScriptLoad() {
@@ -22,42 +23,64 @@ export default class IndexPage extends React.Component {
     const { edges: posts } = data.allMarkdownRemark;
 
     return (
-      <section className="section">
+      <div className="over-all-container">
         <Script
           url="https://identity.netlify.com/v1/netlify-identity-widget.js"
           onLoad={() => this.handleScriptLoad()}
         />
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+
+        <section className="section welcome">
+          <Navbar />
+          
+          <div className="container ">
+            <div className="content">
+              <h1 className="h1">Expolore the future of food with us</h1>
+              <p>
+                Your #1 source for latest news, trends and jobs in cellular agriculture.
+              </p>
+              <input type="email" className="input" placeholder="Your Email" />
+              <button className="btn btn-success">SUBSCRIBE</button>
+              <p>
+                Get the latest research, insights and jobs delivered to your inbox every week.
+              </p>
+            </div>
           </div>
-          {posts
-            .filter(post => post.node.frontmatter.templateKey === "blog-post")
-            .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: "1px solid #eaecee", padding: "2em 4em" }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.frontmatter.path}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.frontmatter.path}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))}
-        </div>
-      </section>
+        </section>
+        <section className="section news">
+          <div className="container ">
+            <div className="content">
+              <h2 className="h1">News & Insights</h2>
+            </div>
+            <div className="container-center feature_list">
+              <a href="https://medium.com/cellagri/january-2018-the-month-in-review-40df065cc1a2" className="feature">
+                <div className="feature_image_container">
+                  <img src={require('../img/blog-image-clean-meat.jpeg')} />
+                </div>
+                <h3 className="feature_headLine">
+                  January 2018: The Month in Review
+                </h3>
+              </a>
+              <a href="https://medium.com/cellagri/cellular-agriculture-the-obstacles-ahead-83ecd77115ac" className="feature">
+                <div className="feature_image_container">
+                  <img src={require('../img/blog-image.jpeg')} />
+                </div>
+                <h3 className="feature_headLine">
+                  Cellular Agriculture: The Obstacles Ahead
+                </h3>
+              </a>
+              <a href="https://medium.com/cellagri/lab-grown-clothing-dd1eef9eafa" className="feature">
+                <div className="feature_image_container">
+                  <img src={require('../img/blog-image-lab-grown.jpeg')} />
+                </div>
+                <h3 className="feature_headLine">
+                  Lab Grown Clothing
+                </h3>
+              </a>
+            </div>
+          </div>
+        </section>
+      </div>
+      
     );
   }
 }
