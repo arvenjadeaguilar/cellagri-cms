ae954d8ca3c7747107efb95a0287a05c7d5f5f5f
arvenjadeaguilar <arvenjadeaguilar@gmail.com>
2018-02-08 22:06:43 +0800

    fix(cms): add cms custom css

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,6 @@
 import CMS from 'netlify-cms';
 import 'netlify-cms/dist/cms.css';
-
+import './cms.css';
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
